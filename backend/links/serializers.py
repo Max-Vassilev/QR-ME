fdfb0c702e030ac2e1a12ea2d8f510555c0574ec
Maxim Vassilev <maximvasilev01@gmail.com>
2025-09-19 15:10:35 +0300

    Put created_at back in the serializer of the link

--- backend/links/serializers.py
+++ backend/links/serializers.py
@@ -4,5 +4,5 @@ from .models import Link
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = ["id", "owner", "platform", "url"]
+        fields = ["id", "owner", "platform", "url", "created_at"]
         extra_kwargs = {"owner": {"read_only": True}}
